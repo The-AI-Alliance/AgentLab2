1de005f17e47fb220b9ea32efca8ce2688f0bff3
Oleh Shliazhko <oleh.shliazhko@servicenow.com>
2025-12-12 12:58:25 +0100

    restore log output
    Signed-off-by: Oleh Shliazhko <oleh.shliazhko@servicenow.com>

--- recipes/hello_miniwob.py
+++ recipes/hello_miniwob.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import time
 
@@ -6,6 +7,11 @@ from agentlab2.benchmarks.miniwob.benchmark import MiniWobBenchmark
 from agentlab2.envs.browser import BrowserEnvConfig
 from agentlab2.experiment import Experiment
 from agentlab2.llm import LLM
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] %(asctime)s - %(name)s:%(lineno)d %(funcName)s() - %(message)s",
+)
 
 debug = False
 
